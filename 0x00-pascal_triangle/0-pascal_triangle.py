2a96b1d23a2508c78c9c6df5ca898f46aee16f4e
Abdurrahman Idris <abdurrahmaneedrees@gmail.com>
2024-06-12 21:33:32 +0100

    Added the pascal_triangle function to generate Pascal's Triangle.

---
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+    0-pascal_triangle.py: pascal_triangle()
+"""
+
+
+def pascal_triangle(n):
+    """
+        returns a lis of lists of integers
+        Args:
+            n (int): number of lists and digits
+        Returns: list of lists
+    """
+    temp_row = [1]
+    temp_l = [0]
+    pTri = []
+
+    
+    return pTri
