84b8b8eb33c6dd05f2c64d6c50434d29120bc53e
Abdurrahman Idris <abdurrahmaneedrees@gmail.com>
2024-07-11 21:15:04 +0100

    feat: Implement N Queens problem solution with proper validation and output formatting
    - Added validation to ensure correct number of command-line arguments
    - Implemented checks for integer type and minimum value of N
    - Improved solution generation and output formatting for N Queens problem
    - Ensured solutions are printed in the required format

--- 0x05-nqueens/0-nqueens.py
+++ 0x05-nqueens/0-nqueens.py
@@ -2,43 +2,38 @@
 """ N queens """
 import sys
 
-
-if len(sys.argv) > 2 or len(sys.argv) < 2:
+if len(sys.argv) != 2:
     print("Usage: nqueens N")
-    exit(1)
+    sys.exit(1)
 
 if not sys.argv[1].isdigit():
     print("N must be a number")
-    exit(1)
-
-if int(sys.argv[1]) < 4:
-    print("N must be at least 4")
-    exit(1)
+    sys.exit(1)
 
 n = int(sys.argv[1])
 
+if n < 4:
+    print("N must be at least 4")
+    sys.exit(1)
+    
 
 def queens(n, i=0, a=[], b=[], c=[]):
-    """ find possible positions """
+    """Find possible positions"""
     if i < n:
         for j in range(n):
             if j not in a and i + j not in b and i - j not in c:
                 yield from queens(n, i + 1, a + [j], b + [i + j], c + [i - j])
     else:
         yield a
-
+        
 
 def solve(n):
-    """ solve """
-    k = []
-    i = 0
-    for solution in queens(n, 0):
-        for s in solution:
-            k.append([i, s])
-            i += 1
-        print(k)
+    """Solve the N Queens problem and print each solution"""
+    for solution in queens(n):
         k = []
-        i = 0
+        for i in range(n):
+            k.append([i, solution[i]])
+        print(k)
 
 
 solve(n)
